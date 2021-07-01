c088fb86d033261b01855346aef91ac9d3bc4abc
patrick richter <patrickrichter@MacBook-Air-von-patrick.local>
2021-07-01 07:21:54 +0200

    Added all necessary functions with proper try catch finally

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,15 @@ const zip = document.getElementById("zip");
 const zipValue = zip.value;
 const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${zipValue}&appid=${apiKey}`
 
+//Data that will be initialized later and 
+//and send with the post request
+let temp = '';
+let date = '';
+let userResponse = '';
 
-
-const postData = async(url, data = []) => {
+// Using Async in favor of chained promises
+// https://blog.devgenius.io/how-do-differences-in-promise-chains-and-async-await-affect-your-code-logic-b85aeb566ebb
+async function postData(url, data = []) {
   console.log('My data frontend', url, data)
 
   const response = await fetch(url, {
@@ -22,23 +28,69 @@ const postData = async(url, data = []) => {
     const newData = await response.json();
     console.log('from try', newData);
     return newData;
-  } catch(error) {
+  }
+  catch (error) {
     console.log('error', error);
   }
   zip.value = "";
 }
 
+
+
 // document.getElementById("btnSendDataToBackend").addEventListener("click", postData);
 document.getElementById("btnSendDataToBackend").addEventListener("click", getWeatherData);
 
 async function getWeatherData() {
-  console.log(weatherUrl);  
-  const response = await fetch(weatherUrl);
+  // const getWeatherData = async() => {
+  console.log(weatherUrl);
+  try {
+    const response = await fetch(weatherUrl);
     const data = await response.json();
-      // return movies;
-      console.log('getWeatherData',data );
+    // return movies;
+    temp = data.main.temp;
+    date = data.dt;
+    userResponse = zipValue;
+    console.log('getWeatherData', data);
   }
+  catch (error) {
+    console.log('Error happened', error);
+  }
+  finally {
+    anotherAsyncFun();
+  }
+}
 
+async function anotherAsyncFun(url = '/newPost', data = { temp, date, userResponse }) {
+
+  //New Post request
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(temp, date, userResponse),
+    });
+  } catch (error) {
+    console.log('Error hapened', error);
+  }
+  finally {
+    updateUi();
+
+  }
+  //After data is successfully send back,
+  //call method to update ui
+}
+
+//update shall retrieve data from our server/app? 
+//select necessary parts in dom and update ui for
+// - Temperature
+// - Date
+// - User input
+async function updateUi() {
+
+}
 
 document.getElementById("btnGenerate").addEventListener("click", generateData)
 
@@ -63,9 +115,8 @@ const getData = async (url) => {
 }
 
 
- postData('/add', {answer:'28'})
- postData('/add', {question:'?'})
+postData('/add', { answer: '28' })
+postData('/add', { question: '?' })
 
 
 
- 
